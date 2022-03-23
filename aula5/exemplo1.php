b953cd3732137ba22a553fafe9afbb3c84df8a6c
CarlosXSU <amigospros354@gmail.com>
2022-03-23 10:42:08 -0300

    exemplo if

---
+++ aula5/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+    $n1=10;
+    $n2=10;
+
+    $media=($n1+$n2)/2;
+
+    if($media >= 7){
+        echo "Você foi aprovado";
+    }
+    else {
+        echo "Você foi reprovado";
+    }
+
+?>
