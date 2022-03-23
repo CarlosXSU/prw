c285b61cb231cf17c5d3d2447dd57be70441bc2f
CarlosXSU <amigospros354@gmail.com>
2022-03-23 11:08:35 -0300

    adicionando ex2

---
+++ aula5/exemplo2.php
@@ -0,0 +1,12 @@
+<?php
+   
+   $op = 'a';
+
+   switch($op){
+       case 'a' : echo "Abrir"; break;
+       case 'a' : echo "Fechar"; break;
+       case 'a' : echo "Salvar"; break;
+       default : echo "Sair"; break;
+   }
+
+?>
