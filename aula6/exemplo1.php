a2b55f78a4c6342bb2442834d959ef35d63decf9
CarlosXSU <amigospros354@gmail.com>
2022-03-30 09:13:21 -0300

    Date (EXEMPLO)

--- aula6/exemplo1.php
+++ aula6/exemplo1.php
@@ -3,6 +3,7 @@
     //exemplos date
     echo "A data atual é: ". date('d/m/y'). "<br>";
     echo "A data atual é: ". date('d/m/Y'). "<br>";
+    echo "A data atual é: ". date('d/M/Y'). "<br>";
     echo "Hoje é: ". date('D'). "<br>";
     echo "Agora são: ". date('H:i:s A');
 
