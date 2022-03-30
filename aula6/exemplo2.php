39a4dbe2d389f9fa4df3fad4d08a94e6fff8aa18
CarlosXSU <amigospros354@gmail.com>
2022-03-30 10:15:32 -0300

    remover espacos e ver quantidade de caracteres EX

---
+++ aula6/exemplo2.php
@@ -0,0 +1,9 @@
+<?php
+
+    $palavra="Ciência da Computação    ";
+    echo "A variável contém: " . strlen($palavra) . " caracteres" . "<br>";
+
+    $palavra=trim($palavra);
+    echo "A variável agora contém: " . strlen($palavra) . " caracteres";
+    
+?>
