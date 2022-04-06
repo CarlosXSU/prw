0e74d0a4c4d3a8d412d868e5b30af2eadee239d7
CarlosXSU <amigospros354@gmail.com>
2022-04-06 09:55:26 -0300

    replace exc 2 lista 2

---
+++ lista3/exc2.php
@@ -0,0 +1,18 @@
+<?php
+
+    $palavra = "Carlos";
+    
+    $novaPal = str_replace('a', 'x', $palavra);
+    $novaPal = str_replace('e', 'x', $novaPal);
+    $novaPal = str_replace('i', 'x', $novaPal);
+    $novaPal = str_replace('o', 'x', $novaPal);
+    $novaPal = str_replace('u', 'x', $novaPal);
+
+    $novaPal = str_replace('A', 'X', $novaPal);
+    $novaPal = str_replace('E', 'X', $novaPal);
+    $novaPal = str_replace('I', 'X', $novaPal);
+    $novaPal = str_replace('O', 'X', $novaPal);
+    $novaPal = str_replace('U', 'X', $novaPal);
+
+    echo $novaPal;
+?>
