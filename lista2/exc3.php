1dff5d99ff63e1ee44ba5a0b8ced251a2807c9b2
CarlosXSU <amigospros354@gmail.com>
2022-04-06 08:32:10 -0300

    exc 3 da lista (PRIMOS)

--- lista2/exc3.php
+++ lista2/exc3.php
@@ -1,17 +1,23 @@
 <?php
 
-    echo "Os números primos entre 0 e 200 são: ";
+    for($num=1; $num<=200; $num++) {
+        $primo = 1;
 
-    for($num=1;$num<=200;$num++){
-        $primo=1;
-        for ($cont=2;$cont<$num;$cont++){
-            if($num%$cont==0){
-                $primo=0;
-                break; 
+        for($i=2; $i<$num; $i++){
+
+            if($num%$i == 0){
+                $primo = 0;
+                break;
             }
-            else{
-            }
-            echo "O número "; $num. " é primo". "<br>";
+
+        }
+
+        if($primo == 1){
+            echo $num." Esse número é primo!<br>";
+        }
+        else{
+            echo $num." Esse número não é primo!<br>";
         }
     }
+
 ?>
