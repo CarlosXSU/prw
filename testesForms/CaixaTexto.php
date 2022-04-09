38a7162f8497c3fcca411c9c4a38a12d65f024c2
CarlosXSU <amigospros354@gmail.com>
2022-04-09 16:08:18 -0300

    testeForms

---
+++ testesForms/CaixaTexto.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <?php
+            $var = $_GET['txtNome'];
+            echo "Seu nome é: " . $var. "<br>";
+            echo "Seu nome é: " . $_GET['txtNome'];
+        ?>
+    </body>
+</html>
