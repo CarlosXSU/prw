563a8fcb9087a7c0b835d82ca77de8a24316e9da
CarlosXSU <amigospros354@gmail.com>
2022-04-11 18:43:55 -0300

    exc1 da lista 4 (form - calculadora)

--- lista4/exc1.php
+++ lista4/exc1.php
@@ -2,47 +2,48 @@
 
     //VERIFICANDO SE TODOS OS CAMPOS FORAM PREENCHIDOS
     //Empty: verifica se determinada string está vazia
+ 
+    $erro = 0;
+
     if(empty($_POST['opt'])){
-        echo "Você não selecionou nenhuma opção, por favor, selecione.";
-        exit;
+        $erro=1;    
     }
-    else{ 
-        if(empty($_POST['N1']) && $_POST['N1'] != 0){
-            echo "Você não digitou o número 1, digite >:(";
-            exit;
-        }
-        else {
-            if(empty($_POST['N2']) && $_POST['N2'] != 0){
-                echo "Você não digitou o número 2, digite >:(";
-                exit;
-            }
-            else{}
-        }
+    if(empty($_POST['N1']) && $_POST['N1'] != 0){
+        $erro=1;    
     }
-
-    $opc = $_POST['opt'];
-    $n1 = $_POST['N1'];
-    $n2 = $_POST['N2'];
-
-    if($opc == 1){
-        echo "A soma dos dois valores é: " . $n1 + $n2; 
+    if(empty($_POST['N2']) && $_POST['N2'] != 0){
+        $erro=1;    
+    }
+    
+    if($erro == 1){
+        echo "Por favor, preencha os campos corretamente.";
     }
     else{
-        if($opc == 2){
-            echo "A subtração dos dois valores é: " . $n1 - $n2; 
+        
+        $opc = $_POST['opt'];
+        $n1 = $_POST['N1'];
+        $n2 = $_POST['N2'];
+
+        if($opc == 1){
+            echo "A soma dos dois valores é: " . $n1 + $n2; 
         }
         else{
-            if($opc == 3){
-                if($n2 != 0){
-                    echo "A divisão dos dois valores é: " . $n1 / $n2;
-                }
-                else{
-                    echo "<h1><span style='color:red'>É impossível dividir por zero, por favor, digite novamente.</span></h1>";
-                }
+            if($opc == 2){
+                echo "A subtração dos dois valores é: " . $n1 - $n2; 
             }
             else{
-                if($opc == 4){
-                    echo "A multiplicação dos dois valores é: " . $n1 * $n2;
+                if($opc == 3){
+                    if($n2 != 0){
+                        echo "A divisão dos dois valores é: " . $n1 / $n2;
+                    }
+                    else{
+                        echo "<h1><span style='color:red'>É impossível dividir por zero, por favor, digite novamente.</span></h1>";
+                    }
+                }
+                else{
+                    if($opc == 4){
+                        echo "A multiplicação dos dois valores é: " . $n1 * $n2;
+                    }
                 }
             }
         }
