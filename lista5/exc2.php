8ff477fcfc53de77546d559747aa006a199ca7ec
CarlosXSU <amigospros354@gmail.com>
2022-04-12 22:40:42 -0300

    exc2 da lista 5 (retornado o preco do produto tb)

---
+++ lista5/exc2.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $produto = $_POST['txtProp'];
+    $produtos = array("faca","console","computador","jogo");
+    $preco = array(20, 5000, 6500, 125);
+
+    $produto = strtolower($produto); //Deixando igual Jogos, Jogos, jogos, joGos...
+
+    $indice = array_search($produto, $produtos);
+
+    if($indice === false){
+        echo "Produto não encontrado :( <br>";
+    }
+    else{
+        echo "produto encontrado, o seu código é: $indice <br>
+              Preço do produto: R$ $preco[$indice]";
+    }
+
+?> 
