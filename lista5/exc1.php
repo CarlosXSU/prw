0acd95d65ba18ec3f7c5c0956e09bf5acde8ae26
CarlosXSU <amigospros354@gmail.com>
2022-04-12 22:33:51 -0300

    exc1 da lista 5 (procurando produto)

---
+++ lista5/exc1.php
@@ -0,0 +1,16 @@
+<?php 
+
+    $produto = $_POST['txtProp'];
+    $produtos = array("faca","console","computador","jogo");
+    $produto = strtolower($produto); //Deixando igual Jogos, Jogos, jogos, joGos...
+
+    $indice = array_search($produto, $produtos);
+
+    if($indice === false){
+        echo "Produto não encontrado :( <br>";
+    }
+    else{
+        echo "produto encontrado, o seu código é: $indice <br>";
+    }
+
+?> 
