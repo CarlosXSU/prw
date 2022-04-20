25b2336e3a0d36cd192358dcd02a4b2f135b38e0
CarlosXSU <amigospros354@gmail.com>
2022-04-20 11:16:09 -0300

    insert eat ate eaten?

---
+++ funny_scripts/script_insert.sql
@@ -0,0 +1,7 @@
+use projeto01;
+
+insert into usuario (nome_usuario, email_usuario, telefone_usuario)
+VALUES ('Murilo Varges', 'murilo.varges@gmal.com','3643-1160');
+
+--mostra a tudo da tabela usuário
+SELECT * from usuario;
