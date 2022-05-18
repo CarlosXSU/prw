f890ab51eb7814265a5e266fc63488708c004d8a
CarlosXSU <amigospros354@gmail.com>
2022-05-17 21:28:57 -0300

    menu principal

--- lista6/index.php
+++ lista6/index.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista6 - Menu principal</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-    
+    <h1 id="menu_text">Menu principal - Lista 6</h1>
+    <div id="menu">
+        <ul>
+            <li><a href="cadastro_agenda.html"> Cadastrar nova agenda</a></li>
+            <li><a href="listar_agenda.php"> Listar agendas cadastradas</a></li>
+        </ul>
+    </div> 
 </body>
 </html>
